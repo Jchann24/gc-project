152cdadaedf07f35c5f7027d1f2f5edccc29e516
Jason Chan <jasonnchann24@gmail.com>
2021-03-01 21:56:57 +0700

    test local json data file

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h1>Test about</h1>
+    <p v-for="x in result" :key="x.title">{{ x }}</p>
   </div>
 </template>
 
 <script>
+import data from 'static/data/test.json'
 export default {
   name: 'AboutPage',
+  asyncData() {
+    let result = JSON.parse(JSON.stringify(data))
+    result = result.data
+    return { result }
+  },
 }
 </script>
 
