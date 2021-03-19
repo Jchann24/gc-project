12d39773bb8ef3b1eda741c6b53b6e306884a1f3
Jason Chan <jasonnchann24@gmail.com>
2021-03-19 16:36:11 +0700

    done banner

---
+++ components/Index/TheCarousel.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div
+      id="carouselExampleFade"
+      class="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div
+          v-for="(imgName, index) in imgList"
+          :key="index"
+          class="carousel-item"
+          :class="{ active: index == 0 }"
+        >
+          <img
+            :src="`/img/banner/${imgName}`"
+            class="d-block mx-auto"
+            :alt="`banner ${imgName}`"
+            loading="lazy"
+          />
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleFade"
+        data-bs-slide="prev"
+      >
+        <i
+          class="ri-arrow-left-s-line c-color-navbar ri-4x"
+          aria-hidden="true"
+        ></i>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleFade"
+        data-bs-slide="next"
+      >
+        <i
+          class="ri-arrow-right-s-line c-color-navbar ri-4x"
+          aria-hidden="true"
+        ></i>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheCarousel',
+  data() {
+    return {
+      imgList: ['banner1.jpg', 'banner3.png', 'banner4.png'],
+    }
+  },
+}
+</script>
+
+<style scoped>
+img {
+  width: 90%;
+  height: 45vh;
+  object-fit: cover;
+  object-position: 50% 50%;
+}
+.carousel-control-prev,
+.carousel-control-next {
+  width: 5%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,11 @@
 <template>
   <div>
-    <div
-      id="carouselExampleFade"
-      class="carousel slide carousel-fade"
-      data-bs-ride="carousel"
-    >
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="..." class="d-block w-100" alt="..." />
-        </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="..." />
-        </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="..." />
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col mx-0 px-0">
+          <IndexTheCarousel />
         </div>
       </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleFade"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleFade"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
     </div>
   </div>
 </template>
