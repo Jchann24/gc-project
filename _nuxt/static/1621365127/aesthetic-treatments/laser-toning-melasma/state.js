d46ea724b25fe25e2463b93edc720ec119c5372c
Jason Chan <jasonnchann24@gmail.com>
2021-05-19 02:12:56 +0700

    24c560f

---
+++ _nuxt/static/1621365127/aesthetic-treatments/laser-toning-melasma/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgc-project\u002F_nuxt\u002Fstatic\u002F1621365127",layout:"default",error:a,serverRendered:true,routePath:"\u002Faesthetic-treatments\u002Flaser-toning-melasma",config:{app:{basePath:"\u002Fgc-project\u002F",assetsPath:"\u002Fgc-project\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9a9f06ca"}}}}(null));
